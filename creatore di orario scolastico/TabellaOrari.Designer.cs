c48590b8ac8dec3bef292b3ded76d3b548835843
Andrea Somaini <andrea.somaini00@gmail.com>
2019-01-31 10:46:10 +0100

    Inserito menu per selezionare la classe. Errore nell'ottenere il nome della classe selezionato, manca l'inserimento nella tabella dell'orario.

--- creatore di orario scolastico/TabellaOrari.Designer.cs
+++ creatore di orario scolastico/TabellaOrari.Designer.cs
@@ -29,32 +29,58 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listaClassi = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 71);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(838, 438);
+            this.dataGridView1.Size = new System.Drawing.Size(838, 379);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Classe";
+            // 
+            // listaClassi
+            // 
+            this.listaClassi.FormattingEnabled = true;
+            this.listaClassi.Location = new System.Drawing.Point(72, 36);
+            this.listaClassi.Name = "listaClassi";
+            this.listaClassi.Size = new System.Drawing.Size(121, 21);
+            this.listaClassi.TabIndex = 2;
+            this.listaClassi.Tag = "Classe";
+            this.listaClassi.SelectionChangeCommitted += new System.EventHandler(this.listaClassi_SelectedIndexChanged);
             // 
             // TabellaOrari
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(862, 453);
+            this.Controls.Add(this.listaClassi);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "TabellaOrari";
             this.Text = "TabellaOrari";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox listaClassi;
     }
 }
--- creatore di orario scolastico/TabellaOrari.cs
+++ creatore di orario scolastico/TabellaOrari.cs
@@ -18,11 +18,35 @@ namespace creatore_di_orario_scolastico
             InitializeComponent();
             orario.creaDatiEsempio();
             orario.creaOrario();
+
+            // Inserisci i nomi delle classi nella combobox
+            
+            foreach (Orario.ClassiRow classe in orario.Classi.Rows )
+            {
+                this.listaClassi.Items.Add( classe[1] );
+            }
         }
 
         // Mostra l'orario sulla Windows Form
-        private void visualizzaOrario()
+        private void visualizzaOrario(String nomeClasse)
         {
+            // Cerca l'id della classe partendo dal nome
+            Int64 trovato = -1;
+            foreach (Orario.ClassiRow classe in orario.Classi.Rows)
+            {
+                if (classe.Nome == nomeClasse)
+                {
+                    trovato = classe.idClaPk;
+                    break;
+                }
+            }
+            if (trovato == -1)
+                throw new Exception("Id della classe non trovato nella visualizzazione orario");
+
+            // Cancella tutto
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
             // Mostra colonne
             dataGridView1.Columns.Add("Ora", "Ora");
             dataGridView1.Columns.Add("Lun", "Lun");
@@ -34,6 +58,10 @@ namespace creatore_di_orario_scolastico
             // Inserisci tutte le righe
             
         }
-        
+
+        private void listaClassi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            visualizzaOrario(listaClassi.SelectedText);
+        }
     }
 }
